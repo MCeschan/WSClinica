2d2278c1aaa2f6f852ca58672de8ce066f0a1b9f
Martina <martinaceschan@gmail.com>
2023-01-20 15:40:41 -0300

    entidades

--- WSClinica/Models/Clinica.cs
+++ WSClinica/Models/Clinica.cs
@@ -1,6 +1,16 @@
-﻿namespace WSClinica.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace WSClinica.Models
 {
     public class Clinica
     {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaInicioActividades { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Habitacion> Habitaciones { get; set; }
     }
 }
--- WSClinica/Models/Habitacion.cs
+++ WSClinica/Models/Habitacion.cs
@@ -1,6 +1,16 @@
-﻿namespace WSClinica.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WSClinica.Models
 {
     public class Habitacion
     {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string Estado { get; set;}
+
+        public int ClinicaID { get; set;}
+        [ForeignKey("ClinicaID")]
+
+        public Clinica Clinica { get; set;}
     }
 }
